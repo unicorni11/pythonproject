2505200427a0a51ca450eb0e136315680e9d3b5f
Unicorni11 <100362690+unicorni11@users.noreply.github.com>
2022-05-18 16:12:28 +0200

    First commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-import daten
 import json
 from datetime import datetime
 
@@ -15,21 +14,21 @@ def start():
     name = "Anna-Lena"
     return render_template("index.html", name=name)
 
-@app.route("/shop")
-def schmuckshop():
-
-    return render_template("shop.html")
 
 @app.route("/formular", methods=['GET', 'POST']) #Verlinkung Formular
 def formular():
+    daten_uebermittelt = False
     if request.method == 'POST':
         data = request.form
         name = data["name"]
+        adresse = data["adresse"]
         was = data.get("was")
         welches = data.get("welches")
         anzahl = data.get("anzahl")
         gravur = data["gravur"]
         zeitstempel = datetime.now()
+
+        daten_uebermittelt = True;
 
         try:
             with open("bestellung.json", "r") as open_file:  # r für read = lesen
@@ -37,15 +36,47 @@ def formular():
         except FileNotFoundError:
             datei_inhalt = []
 
-        my_dict = {"Name": name, "Was": was, "Welches": welches, "Anzahl": anzahl, "Gravur": gravur, "Zeitstempel": zeitstempel}
+        my_dict = {"Name": name, "Adresse": adresse, "Was": was, "Welches": welches, "Anzahl": anzahl, "Gravur": gravur, "Zeitstempel": zeitstempel}
         datei_inhalt.append(my_dict)
 
         with open("bestellung.json", "w") as open_file:
             json.dump(datei_inhalt, open_file, indent=4, default=str)  # indent=4 das es schöne aussieht
-        return str("Danke für deine Eingabe, die Daten wurden gespeichert.")
+        return render_template("formular.html", daten_uebermittelt=daten_uebermittelt)
     else:
         return render_template("formular.html")
 
+
+@app.route("/bestellungen")
+def bestellungen():
+    bestell_uebersicht = []
+
+    try:
+        with open("bestellung.json", "r") as open_file:  # r für read = lesen
+            datei_inhalt = json.load(open_file)
+    except FileNotFoundError:
+        datei_inhalt = []
+
+    for element in datei_inhalt:
+        if element["Gravur"] == "":
+            gravurwert = "keine Gravur"
+        else:
+            gravurwert = element["Gravur"]
+
+        bestell_uebersicht.append([element["Name"], element["Was"], element["Welches"], element["Anzahl"], gravurwert])
+
+
+    return render_template("bestellungen.html", bestell_uebersicht=bestell_uebersicht)
+
+
+@app.route("/backend")
+def backend():
+
+    return render_template("backend.html")
+
+@app.route("/visualisierung")
+def visualisierung():
+
+    return render_template("visualisierung.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
